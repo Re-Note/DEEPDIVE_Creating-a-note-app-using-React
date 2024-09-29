c14b3f0664c59cb9fdb7bf2eba44db4a3872abc9
Re-Note <dldydtn207@gmail.com>
2024-09-29 19:35:33 +0900

    feat: build 설정 작성

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/DEEPDIVE_Creating-a-note-app-using-React/', // GitHub Pages 경로 설정
+});
