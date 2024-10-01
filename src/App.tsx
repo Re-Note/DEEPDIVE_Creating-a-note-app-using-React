844ee263c30f968ad746f3ece7a1670d27830e70
Re-Note <dldydtn207@gmail.com>
2024-10-01 16:47:23 +0900

    feat: deployment 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'; // HashRouter로 변경
-import { ToastContainer } from 'react-toastify';
-import './App.css';
-import { CreateNoteModal, TagsModal } from './components';
-import { useAppSelector } from './hooks/redux';
-import { Navbar, Sidebar } from './layout';
-import { AllNotes, ArchiveNotes, ErrorPage, TagNotes, TrashNotes } from './pages';
-import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import './App.css'
+import { CreateNoteModal, TagsModal } from './components'
+import { useAppSelector } from './hooks/redux'
+import { Navbar, Sidebar } from './layout'
+import { AllNotes, ArchiveNotes, ErrorPage, TagNotes, TrashNotes } from './pages'
+import 'react-toastify/dist/ReactToastify.css'
+
 
 function App() {
-  const { viewEditTagsModal, viewCreateNoteModal } = useAppSelector((state) => state.modal);
+
+  const { viewEditTagsModal, viewCreateNoteModal } = useAppSelector(state => state.modal);
 
   return (
     <div className="app">
+
       {viewCreateNoteModal && <CreateNoteModal />}
       {viewEditTagsModal && <TagsModal type='edit' />}
 
@@ -22,8 +25,7 @@ function App() {
         autoClose={1500}
       />
 
-      {/* BrowserRouter를 HashRouter로 변경 */}
-      <HashRouter>
+      <BrowserRouter>
         <Sidebar />
         <div className='app__container'>
           <Navbar /> 
@@ -36,9 +38,9 @@ function App() {
             <Route path='/*' element={<Navigate to={"/404"} />} />
           </Routes>
         </div>
-      </HashRouter>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
